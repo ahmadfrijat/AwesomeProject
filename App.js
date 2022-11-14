ddd1651cc84f2815a385d50efed7e097ba37aace
ahmadfrijat <ahmadalfrijat7@gmail.com>
2022-11-14 12:51:49 +0200

    theme

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
 import * as React from 'react';
 import { StatusBar as ExpoStatusBar, StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
-import { Searchbar } from 'react-native-paper';
 import { RestaurantsScreen } from './src/features/restaurants/screens/resturants.screen';
+import { ThemeProvider } from 'react-native-paper';
+import { theme } from './src/infrastructure/theme';
 
-// const isAndroid = Platform.OS === 'android';
+
+
 
 export default function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
     <RestaurantsScreen/>
+    </ThemeProvider>
     <ExpoStatusBar style='auto' />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
+
--- src/features/restaurants/screens/resturants.screen.js
+++ src/features/restaurants/screens/resturants.screen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StatusBar as ExpoStatusBar, StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 import { Searchbar } from 'react-native-paper';
-import { RestaurantInfo } from '../components/restaurant-info.component';
+import { RestaurantInfoCard } from '../components/restaurant-info.card';
 
 
 export const RestaurantsScreen = () => (
@@ -10,8 +10,8 @@ export const RestaurantsScreen = () => (
     <View style={{ padding: 16}}>
     <Searchbar placeholder="Search"/>
     </View>
-    <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-     <RestaurantInfo/>
+    <View style={{ flex: 1, padding: 16 }}>
+     <RestaurantInfoCard/>
     </View>
   </SafeAreaView>
 )
